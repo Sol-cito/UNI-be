3027c19aa72eaddf0d73975f6ca1437f7238296a
sol <dataenggu@gmail.com>
2023-04-02 18:31:47 +0200

    Adjust script name and adjust docker-image-build.sh

--- devscript.sh
+++ devscript.sh
@@ -4,8 +4,10 @@ ARG=${1}
 
 if [ "${ARG}" == "" ]; then
   echo "[Script error] Argument is needed!!"
-elif [ "${ARG}" == "img" ]; then
-  sh ./script/docker-image-build.sh
+elif [ "${ARG}" == "dkimgbd" ]; then
+  sh ./script/docker-image-build.sh build
+elif [ "${ARG}" == "dkimgrm" ]; then
+  sh ./script/docker-image-build.sh remove
 else
   echo "[Script error] Argument '${ARG}' is not defined in the script"
 fi
--- script/docker-image-build.sh
+++ script/docker-image-build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-./gradlew build
+ARG=${1}
 
-docker build -f Dockerfile.dev -t solcito/uni-be .
+if [ "${ARG}" == "build" ]; then
+  ./gradlew build
 
-docker run -d -t --name uni-be -p 8081:8081 solcito/uni-be:latest
+  docker build -f Dockerfile.dev -t solcito/uni-be .
+
+  docker run -d -t --name uni-be -p 8081:8081 solcito/uni-be:latest
+
+  docker push solcito/uni-be:latest
+
+elif [ "${ARG}" == "remove" ]; then
+  docker stop uni-be
+
+  docker rm uni-be
+
+  docker rmi solcito/uni-be
+else
+  echo "[ERROR] check docker-image-build arg!"
+fi
+
