8dc7e6f950ccbe38289c700f0c6436336af77c5d
sol <dataenggu@gmail.com>
2023-04-02 18:21:16 +0200

    Adjust Docker script and build files

--- script/docker-image-build.sh
+++ script/docker-image-build.sh
@@ -2,6 +2,6 @@
 
 ./gradlew build
 
-docker build -f Dockerfile.dev -t uni-be .
+docker build -f Dockerfile.dev -t solcito/uni-be .
 
-docker run -d --name uni-be -p 8081:8081 uni-be
+docker run -d -t --name uni-be -p 8081:8081 solcito/uni-be:latest
